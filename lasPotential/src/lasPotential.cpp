127233a45ebb176019da4035e28d4f7fdff9929f
Jamie M Dowden-Duarte <152818382+Goober0415@users.noreply.github.com>
2025-12-19 09:40:29 -0700

    121920250940

--- lasPotential/src/lasPotential.cpp
+++ lasPotential/src/lasPotential.cpp
@@ -1,84 +1,42 @@
-/*
+/* 
  * Project lasPotential
  * Author: Jamie Dowden-Duarte
  * Date: 11-12-2025
- * laser and photodiode functionality with neopixels
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
 
 #include "Particle.h"
-#include "neopixel.h"
-#include "Colors.h"
 
 ///////////////
-/// Global//////
+///Global//////
 ///////////////
 const int LAS = D3;
-const int PHOTO = A0;
-const int PIXELCOUNT = 12;
+const int PHOTO = D16;
 
 int reading, prevRead;
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-///////////////
-/// OBJECTS//////
-///////////////
-Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
-
-void setup()
-{
+void setup() {
   Serial.begin();
-  waitFor(Serial.isConnected, 10000);
+  waitFor(Serial.isConnected,10000);
 
   /////////
-  // pinMode
+  //pinMode
   /////////
-  pinMode(LAS, OUTPUT);
-  pinMode(PHOTO, INPUT);
-
-  /////////
-  // neopixel
-  /////////
-  pixel.begin();
-  pixel.setBrightness(10);
-  for (int i = 0; i < PIXELCOUNT; i++)
-  {
-    pixel.setPixelColor(i, blue);
-    pixel.show();
-  }
+  pinMode(LAS,OUTPUT);
+  pinMode(PHOTO,INPUT);
 }
 
-void loop()
-{
-  if (reading > 38)
-  {
-    for (int i = 0; i < PIXELCOUNT; i++)
-    {
-      pixel.setPixelColor(i, yellow);
-      pixel.show();
-    }
-  }
-  else
-  {
-    for (int i = 0; i < PIXELCOUNT; i++)
-    {
-      pixel.setPixelColor(i, blue);
-      pixel.show();
-    }
-  }
-  digitalWrite(LAS, HIGH);
-  reading = analogRead(PHOTO);
-  delay(10000);
 
-  // Serial.printf("read %i\n",reading);
-  // delay(2000);
+void loop() {
+digitalWrite(LAS,HIGH);
 
-  digitalWrite(LAS, LOW);
-  reading = analogRead(PHOTO);
-  delay(10000);
+reading = analogRead(PHOTO);
+prevRead = 0;
+if(reading != prevRead){
+Serial.printf("reading %i",reading);
 
-  // Serial.printf("read %i\n",reading);
-  // delay(2000);
+}
 }
